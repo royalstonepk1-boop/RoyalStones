f4c241f0ba34de82a80af4d2a52f042490420008
royalstonepk1-boop <royalstonepk1@gmail.com>
2026-01-27 01:30:58 +0500

    Url changes done

--- royalstones-frontend/src/api/backend.js
+++ royalstones-frontend/src/api/backend.js
@@ -1,4 +1,5 @@
 // const BACKEND_URL = "http://localhost:4000/api";
 // const BACKEND_URL = "http://192.168.100.5:4000/api";
-const BACKEND_URL = "https://royalstones.onrender.com/api";
+const BACKEND_URL = "https://lightpink-porpoise-274044.hostingersite.com/api";
+// const BACKEND_URL = "https://royalstones.onrender.com/api";
 export default BACKEND_URL;
